3d8c8474714ef5921401ed7fb57f2843bb534b62
zsz <zsz@qq.com>
2020-08-09 16:08:55 +0800

    本次修改

--- src/com/zsz/User.java
+++ src/com/zsz/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("第一個程序");
         System.out.println("第二次提交");
+        System.out.println("本次修改");
     }
 }
